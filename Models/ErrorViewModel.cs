3090d4f1a306e05cdab14b387ea4e35c70e6b1df
Damaris <damaris_dolha@yahoo.com>
2022-10-11 18:55:03 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dolha_Damaris_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
