2e6398e4fe39d4586b437c37e8448dce07e03bbe
Damarissss <damaris_dolha@yahoo.com>
2022-10-11 19:41:14 +0300

    Metoda Index returneaza un string ("Bine ati venit!"). Metoda Index2 returneaza un string reprezentand un salut("Hello!"). Valorile din cadrul metodei Index3 vor fi afisate utilizatorului impreuna cu un mesajul "Valoarea lui x este: x. Valoarea lui n este:: n".

---
+++ Controllers/HomeController2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dolha_Damaris_Lab1.Controllers
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Bine ati venit!");
+        }
+        public IActionResult Index2()
+        {
+            return Content("Hello!");
+        }
+        public IActionResult Index3(string x, int n)
+        {
+            return Content("Valoarea lui x este: " + x + ". Valoarea lui n este: " + n);
+        }
+    }
+}
